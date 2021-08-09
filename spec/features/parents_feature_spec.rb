01a69da44f909be8701b49947a078addb0c305a3
Kevin Cuadros <kev040798@gmail.com>
2021-08-08 20:49:56 -0400

    Create parent show page functionality with test

--- spec/features/parents_feature_spec.rb
+++ spec/features/parents_feature_spec.rb
@@ -4,20 +4,36 @@ describe 'Parent' do
   describe 'As a visitor' do
     describe 'When I visit parents index page' do
       it 'should see the name of each parent record in the system' do
-        user1 = create(:parent)
-        user2 = create(:parent)
-        user3 = create(:parent)
-        user4 = create(:parent)
-        user5 = create(:parent)
+        parent1 = create(:parent)
+        parent2 = create(:parent)
+        parent3 = create(:parent)
+        parent4 = create(:parent)
+        parent5 = create(:parent)
 
         visit '/parents'
 
         expect(page).to have_content('Parents')
-        expect(page).to have_content(user1.name)
-        expect(page).to have_content(user2.name)
-        expect(page).to have_content(user3.name)
-        expect(page).to have_content(user4.name)
-        expect(page).to have_content(user5.name)
+        expect(page).to have_content(parent1.name)
+        expect(page).to have_content(parent2.name)
+        expect(page).to have_content(parent3.name)
+        expect(page).to have_content(parent4.name)
+        expect(page).to have_content(parent5.name)
+      end
+    end
+
+    describe 'When I visit Parent show page' do
+      it 'should see parent ID and its attributes' do
+        parent = create(:parent)
+
+        visit "/parents/#{parent.id}"
+
+        expect(page).to have_content("Parent: #{parent.id}")
+
+        within '.parent-attrs' do
+          expect(page).to have_content("Name: #{parent.name}")
+          expect(page).to have_content("Age: #{parent.age}")
+          expect(page).to have_content("Married: #{parent.married}")
+        end
       end
     end
   end
