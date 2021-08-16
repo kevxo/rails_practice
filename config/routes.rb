3412ac5d18252b06cd7eddcc622a8c666be5821f
Kevin Cuadros <kev040798@gmail.com>
2021-08-15 20:38:35 -0400

    Create route for new parent form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :parents, only: %i[index show]
+  resources :parents, only: %i[index new show]
   resources :childrens, only: %i[index show]
 
   get '/parents/:parent_id/childrens', to: 'parent_child#index'
