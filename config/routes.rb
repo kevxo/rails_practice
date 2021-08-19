99ca1ce1a614784e06ce4a743dc4beec4fc7d630
Kevin Cuadros <kev040798@gmail.com>
2021-08-19 12:29:01 -0400

    Create the edit and update route for child

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :parents, only: %i[index new create show edit update]
-  resources :childrens, only: %i[index show]
+  resources :childrens, only: %i[index show edit update]
 
   get '/parents/:parent_id/childrens', to: 'parent_child#index'
   get '/parents/:parent_id/childrens/new', to: 'parent_child#new'
